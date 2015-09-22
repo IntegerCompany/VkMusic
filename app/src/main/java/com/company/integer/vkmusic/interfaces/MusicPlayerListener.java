951578406ad6ee389612b0ca5a81ccc5656876ea
Andriy <AndriyTheProgrammer@gmail.com>
2015-09-22 15:26:03 +0300

    Before merge. Added documentation.

--- app/src/main/java/com/company/integer/vkmusic/interfaces/MusicPlayerListener.java
+++ app/src/main/java/com/company/integer/vkmusic/interfaces/MusicPlayerListener.java
@@ -7,9 +7,20 @@ import com.company.integer.vkmusic.pojo.MusicTrackPOJO;
  */
 public interface MusicPlayerListener {
 
+    /**
+     * Playlist end. Probably you need to call "uploadMore()" here
+     */
     void endOfPlaylist();
 
+    /**
+     * Returns buffering progress for current track
+     * @param percent buffering progress
+     */
     void onPlayerTrackUpdating(int percent);
 
+    /**
+     * Called when current track was changed by any reason
+     * @param musicTrack current track
+     */
     void onCurrentTrackChanged(MusicTrackPOJO musicTrack);
 }
--- app/src/main/java/com/company/integer/vkmusic/interfaces/TracksLoaderListener.java
+++ app/src/main/java/com/company/integer/vkmusic/interfaces/TracksLoaderListener.java
@@ -11,7 +11,16 @@ public interface TracksLoaderListener {
 
     int MY_TRACKS = 1, RECOMMENDATIONS = 2, SAVED = 3, SEARCH = 4;
 
+    /**
+     * Called when VK returned info about tracks
+     * @param newPlaylist tracks
+     * @param queryType from which source comes playlist
+     */
     void tracksLoaded(ArrayList<MusicTrackPOJO> newPlaylist, int queryType);
 
+    /**
+     * Called when something went wrong
+     * @param errorMessage
+     */
     void tracksLoadingError(String errorMessage);
 }
